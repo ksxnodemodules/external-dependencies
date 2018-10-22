b929c335e2e89e3a31cfbd45d64c1cb0648597d4
khai96_ <hvksmr1996@gmail.com>
2018-10-22 17:21:30 +0700

    Move modules out of lib/

--- packages/require-external-programs-lib/index.ts
+++ packages/require-external-programs-lib/index.ts
@@ -1,3 +1,7 @@
-export * from './lib'
-import { fromAllManifests } from './lib'
+export * from './types'
+export * from './constants'
+export * from './enums'
+export * from './classes'
+export * from './functions'
+import { fromAllManifests } from './functions'
 export default fromAllManifests
