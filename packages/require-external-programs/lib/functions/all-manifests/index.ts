119aa4b5971f24b7e7e9c395827a3cf573fa4fc0
khai96_ <hvksmr1996@gmail.com>
2018-10-22 14:26:43 +0700

    Fix getAllManifests

--- packages/require-external-programs/lib/functions/all-manifests/index.ts
+++ packages/require-external-programs/lib/functions/all-manifests/index.ts
@@ -1,8 +1,9 @@
+import path from 'path'
 import glob from 'glob-promise'
 import { ManifestList } from '../../types'
 
 function getAllManifests (dirname: string): Promise<ManifestList> {
-  return glob('**/package.json', { root: dirname })
+  return glob(path.join(dirname, '**/package.json'))
 }
 
 export = getAllManifests
