0bb56b12570b848887b67bf46e1792e9fa1edc4e
khai96_ <hvksmr1996@gmail.com>
2018-10-24 17:45:51 +0700

    Attempt to fix ts-jest's annoyance without useless function call

--- packages/require-external-programs-db/functions/command-exists/index.ts
+++ packages/require-external-programs-db/functions/command-exists/index.ts
@@ -1,6 +1,2 @@
 import { dbCommands } from '../../database'
 export = (command: string) => Boolean(dbCommands.calculate(command))
-
-// Stupid command to workaround ts-jest's stupidity
-// @ts-ignore
-; (() => undefined)(dbCommands)
