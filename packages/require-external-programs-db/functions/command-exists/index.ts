ee00c42c96bc2ddf305ad48c1ffe9c215b9ee8d6
khai96_ <hvksmr1996@gmail.com>
2018-10-24 18:04:12 +0700

    Add explanation to a horrific atrocity

--- packages/require-external-programs-db/functions/command-exists/index.ts
+++ packages/require-external-programs-db/functions/command-exists/index.ts
@@ -1,7 +1,9 @@
 import { dbCommands } from '../../database'
 export = (command: string) => Boolean(dbCommands.calculate(command))
 
-// Stupid statement to workaround ts-jest's stupidity
+// Stupid statement to workaround ts-jest's stupidity:
+//   - Without this statement, ts-jest will not emit 'database_1'
+//   - DO NOT remove this
 // @ts-ignore
 // tslint:disable-next-line:no-unused-expression
 ; dbCommands
