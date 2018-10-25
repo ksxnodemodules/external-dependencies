40f944db1683b4a290986dac472ce281f7dd9c10
khai96_ <hvksmr1996@gmail.com>
2018-10-25 15:17:51 +0700

    Track more constants

--- test/require-external-programs/constants.test.ts
+++ test/require-external-programs/constants.test.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import changeCase from 'change-case'
 import { scripts } from 'require-external-programs'
 import { bin } from 'require-external-programs-bin'
@@ -9,11 +10,45 @@ describe('require-external-programs', () => {
       install: require.resolve('require-external-programs/install')
     })
   })
+
+  describe('package.json', () => {
+    const manifest = require('require-external-programs/package.json')
+
+    it('has scripts', () => {
+      expect(manifest).toHaveProperty('scripts')
+      expect(typeof manifest.scripts).toBe('object')
+    })
+
+    it('has scripts.install', () => {
+      expect(manifest.scripts.install).toMatchSnapshot()
+    })
+
+    it('does not have bin', () => {
+      expect(manifest).not.toHaveProperty('bin')
+    })
+  })
 })
 
 describe('require-external-programs-bin', () => {
   it('bin', () => {
     expect(bin).toBe(require.resolve('require-external-programs-bin/bin'))
+  })
+
+  describe('package.json', () => {
+    const manifest = require('require-external-programs-bin/package.json')
+
+    it('has bin', () => {
+      expect(manifest).toHaveProperty('bin')
+    })
+
+    it(`has bin["${KEYWORD}"]`, async () => {
+      expect(manifest.bin).toHaveProperty(KEYWORD)
+
+      expect(path.resolve(
+        path.dirname(require.resolve('require-external-programs-bin/package.json')),
+        manifest.bin[KEYWORD]
+      )).toBe(bin)
+    })
   })
 })
 
