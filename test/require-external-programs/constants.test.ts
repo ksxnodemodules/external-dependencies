8b3297b864d76b9de08c23e2889347a2e7a022bc
khai96_ <hvksmr1996@gmail.com>
2018-10-25 14:56:03 +0700

    Group into one describe

--- test/require-external-programs/constants.test.ts
+++ test/require-external-programs/constants.test.ts
@@ -1,10 +1,12 @@
 import changeCase from 'change-case'
 import { KEYWORD, KEYNAME } from 'require-external-programs-lib'
 
-it('KEYWORD matches package name', () => {
-  expect(KEYWORD).toBe('require-external-programs')
-})
+describe('require-external-programs-lib', () => {
+  it('KEYWORD matches package name', () => {
+    expect(KEYWORD).toBe('require-external-programs')
+  })
 
-it('KEYNAME is lower camel case of KEYWORD', () => {
-  expect(KEYNAME).toBe(changeCase.camel(KEYWORD))
+  it('KEYNAME is lower camel case of KEYWORD', () => {
+    expect(KEYNAME).toBe(changeCase.camel(KEYWORD))
+  })
 })
