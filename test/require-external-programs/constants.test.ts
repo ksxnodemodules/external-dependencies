dfea63b2551278594e9dc2cbd6144bd14164530d
khai96_ <hvksmr1996@gmail.com>
2018-10-25 15:00:36 +0700

    Track bin and scripts

--- test/require-external-programs/constants.test.ts
+++ test/require-external-programs/constants.test.ts
@@ -1,5 +1,21 @@
 import changeCase from 'change-case'
+import { scripts } from 'require-external-programs'
+import { bin } from 'require-external-programs-bin'
 import { KEYWORD, KEYNAME } from 'require-external-programs-lib'
+
+describe('require-external-programs', () => {
+  it('scripts', () => {
+    expect(scripts).toEqual({
+      install: require.resolve('require-external-programs/install')
+    })
+  })
+})
+
+describe('require-external-programs-bin', () => {
+  it('bin', () => {
+    expect(bin).toBe(require.resolve('require-external-programs-bin/bin'))
+  })
+})
 
 describe('require-external-programs-lib', () => {
   it('KEYWORD matches package name', () => {
