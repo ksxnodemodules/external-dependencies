04219360c0293f898b6f06e6252a7900bcb6e443
khai96_ <hvksmr1996@gmail.com>
2018-10-28 22:06:58 +0700

    Use $INIT_CWD instead

--- packages/require-external-programs-bin/bin/index.js
+++ packages/require-external-programs-bin/bin/index.js
@@ -3,7 +3,7 @@ const process = require('process')
 const { execute } = require('require-external-programs-utils')
 
 execute(
-  process.cwd(),
+  process.env.INIT_CWD,
   error => console.error(error),
   status => process.exit(status)
 )
