f115cc221ce33db5d928f2932a11bb8ca6392ad6
khai96_ <hvksmr1996@gmail.com>
2018-10-29 13:32:20 +0700

    Attempt at shortening paths

--- packages/require-external-programs-bin/bin/index.js
+++ packages/require-external-programs-bin/bin/index.js
@@ -2,8 +2,10 @@
 const process = require('process')
 const { execute } = require('require-external-programs-utils')
 
+process.chdir(process.env.INIT_CWD)
+
 execute(
-  process.env.INIT_CWD,
+  '.',
   error => console.error(error),
   status => process.exit(status)
 )
